f90a933ca820af9c5190b7d0bb79483885a8aa79
FabiCasC <fabiana.castroc22@gmail.com>
2024-12-24 22:07:48 -0500

    change in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -17,7 +17,7 @@
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
             <div className="flex-shrink-0 flex items-center">
-                <img className="h-8 w-auto" src="/public/vite.svg" alt="Bey-Max Logo" />
+                <img className="h-8 w-auto" src="/vite.svg" alt="Bey-Max Logo" />
             </div>
             <div className="hidden md:flex items-center">
                 {navItems.map((item) => (
