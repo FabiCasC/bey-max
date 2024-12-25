83dc2151060da16d9b6877eca03fea661d912eb8
FabiCasC <fabiana.castroc22@gmail.com>
2024-12-24 21:46:20 -0500

    responsive and blank spaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
-import { Navbar } from "./components/NavBar";
-import { PrincipalSection } from "./components/PrincipalSection";
-import { HeroSection } from './components/HeroSection';
-import { ServicesSection } from './components/ServicesSection';
-import { FeaturesGrid } from './components/FeaturesGrid';
-import { BenefitsSection } from './components/BenefitsSection';
-import { InterestForm } from './components/InteresForm';
-import { Footer } from './components/Footer';
+import { Navbar } from "./components/NavBar"
+import { PrincipalSection } from "./components/PrincipalSection"
+import { HeroSection } from './components/HeroSection'
+import { ServicesSection } from './components/ServicesSection'
+import { FeaturesGrid } from './components/FeaturesGrid'
+import { BenefitsSection } from './components/BenefitsSection'
+import { InterestForm } from './components/InteresForm'
+import { Footer } from './components/Footer'
 
 function App() {
   return (
-    <main className="w-full">
-      <div className="h-[550px]">
-        <Navbar />
-        <PrincipalSection />
-      </div>
-      <div className="max-h-[600px]">
-        <HeroSection />
-      </div>
-        <ServicesSection />
-      <div>
-        <FeaturesGrid />
-      </div>
-      <div className="min-h-screen">
-        <BenefitsSection />
-      </div>
-        <InterestForm />
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <div className="py-8 md:py-12 lg:py-16">
+          <PrincipalSection />
+        </div>
+        <div className="py-1 md:py-1 lg:py-1">
+          <HeroSection />
+        </div>
+        <div className="pt-1 md:pt-1 lg:pt-1">
+          <ServicesSection />
+        </div>
+        <div>
+          <FeaturesGrid />
+        </div>
+        <div className="py-6 md:py-8 lg:py-10">
+          <BenefitsSection />
+        </div>
+        <div className="py-6 md:py-8 lg:py-10">
+          <InterestForm />
+        </div>
+      </main>
       <Footer />
-    </main>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
+
