1a7d7ff263e912c29f545e184ff210120a1f113e
FabiCasC <fabiana.castroc22@gmail.com>
2024-12-24 22:01:40 -0500

    changes for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+// Configurar __dirname para ESM
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Esto asegura que @ apunte a la carpeta 'src'
+      '@': resolve(__dirname, 'src'), // Esto asegura que @ apunte a la carpeta 'src'
     },
   },
 })
